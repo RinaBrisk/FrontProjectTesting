c902bfb64aba377b71c68a461355c15e4c579a2f
Rina <rina_sergeeva@bk.ru>
2020-05-13 14:36:55 +0300

    first commit

---
+++ static-file-server.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+const request = require('request');
+
+const host = '127.0.0.1'
+const port = 3000
+
+router.get('', function(req,res){
+	res.sendFile(path.join(__dirname + '/front.html'));
+});
+
+app.use(express.static(__dirname + '/'));
+app.use('/', router);
+
+
+app.listen(port, host, function() {
+  console.log(`Server listens http://${host}:${port}`)
+})
