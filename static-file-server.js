b738fa96a25d55e7b9a148d906432eac0fe44120
Rina <rina_sergeeva@bk.ru>
2020-05-15 22:47:43 +0300

    fixed

--- static-file-server.js
+++ static-file-server.js
@@ -7,11 +7,24 @@ const router = express.Router();
 const host = '127.0.0.1'
 const port = 3000
 
+const path_quality_report = 'C:/Users/Rina/.jenkins/workspace/CodeQuality/target/site';
+const path_coverage_report = 'C:/Users/Rina/.jenkins/workspace/TestCoverage/target/site/cobertura';
+
 router.get('', function(req,res){
 	res.sendFile(path.join(__dirname + '/front.html'));
+	app.use(express.static(__dirname + '/'));
 });
 
-app.use(express.static(__dirname + '/'));
+router.get('/qualityReport', function(req,res){
+	res.sendFile(path.join(path_quality_report + '/checkstyle.html'));
+	app.use(express.static(path_quality_report + '/'));
+});
+
+router.get('/coverageReport', function(req,res){
+	res.sendFile(path.join(path_coverage_report + '/index.html'));
+	app.use(express.static(path_coverage_report + '/'));
+});
+
 app.use('/', router);
 
 app.listen(port, host, function() {
